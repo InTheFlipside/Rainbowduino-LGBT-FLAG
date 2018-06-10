24f1a13c13573704c285c19aa79f96c67f622df6
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2018-06-10 10:55:35 +0200

    Create Code.ino

---
+++ Code.ino
@@ -0,0 +1,82 @@
+ 
+ #include <Rainbowduino.h>
+
+void setup()
+{
+  Rb.init(); //initialize Rainbowduino driver
+}
+unsigned int v; 
+unsigned int z,x,y;
+unsigned int c; 
+void loop()
+{
+ 
+ for(x=0;x<4;x++)
+  {
+       delay(20);
+    for(y=0;y<4;y++)
+    {
+      delay(20);
+      for(z=0;z<4;z++) {
+     Rb.setPixelZXY(z,x,y,0xff0000); //RED 
+      }
+    }
+  }  
+  
+
+   for(x=0;x<4;x++)
+  {
+       delay(20);
+    for(y=0;y<4;y++)
+    {
+      delay(20);
+      for(z=0;z<4;z++) {
+     Rb.setPixelZXY(z,x,y,0xe6e600); // YELLOW
+      }
+    }
+  }  
+
+
+
+   for(x=0;x<4;x++)
+  {
+       delay(20);
+    for(y=0;y<4;y++)
+    {
+      delay(20);
+      for(z=0;z<4;z++) {
+     Rb.setPixelZXY(z,x,y,0x00e600); // GREEN
+      }
+    }
+  }
+
+  
+  
+//  delay(100);
+  // Rb.blankDisplay();
+
+
+ for(x=0;x<4;x++)
+  {
+       delay(20);
+    for(y=0;y<4;y++)
+    {
+      delay(20);
+      for(z=0;z<4;z++) {
+     Rb.setPixelZXY(z,x,y,0x33bbff); // BLUE
+      }
+    }
+  }  
+
+
+ for(x=0;x<4;x++)
+  {
+       delay(20);
+    for(y=0;y<4;y++)
+    {
+      delay(20);
+      for(z=0;z<4;z++) {
+     Rb.setPixelZXY(z,x,y,0xcc33ff); // Purple
+      }
+    }
+  }  
